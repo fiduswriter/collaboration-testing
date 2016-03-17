d59482b0f7da59c6705d4a744c9dd2e0626f7a4b
Johannes Wilm <mail@johanneswilm.org>
2016-03-17 02:47:26 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+HOWTO
+======
+
+1. Download Chrome driver selenium and put in the same directory
+
+2. Enter the virtual fiduswriter environment (or create a new one)
+
+3. Install python selenium bindings:
+
+    pip install selenium
+
+4. Copy test-driver.py to testing.py
+
+5. Fill in the server details at the top of testing.py
+
+6. python testing.py
---
+++ test-driver.py
@@ -0,0 +1,36 @@
+SERVER_ADDRESS = ''
+USER_ID = ''
+USER_PW = ''
+
+TEST_TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lobortis tellus urna, quis semper ante tristique sodales. Vestibulum bibendum ligula elit, eget ultricies turpis finibus sit amet. Integer bibendum neque in aliquam vehicula. Donec finibus lacinia mauris et dictum. Ut interdum, nibh ut finibus finibus, sapien nisi pretium nulla, non scelerisque tortor tortor a nunc. Etiam feugiat metus ac varius sagittis. Nam nec lectus consectetur, posuere dolor et, tincidunt tellus. Aenean augue mauris, porta eu scelerisque quis, elementum at nunc. Aliquam malesuada erat pellentesque enim aliquet, scelerisque auctor neque dapibus. Vivamus justo sem, efficitur nec elit nec, malesuada congue lectus. Morbi ipsum turpis, cursus eget arcu non, ultricies faucibus lorem. Suspendisse est dui, tristique ac elit non, maximus condimentum justo. Sed sit amet risus sapien. Phasellus hendrerit efficitur rhoncus. Etiam in consectetur dui. Sed fermentum mauris ut metus molestie, ac porttitor ex ullamcorper."
+
+import time
+from selenium import webdriver
+from random import randrange
+
+
+driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
+driver.implicitly_wait(10) # seconds
+driver.get(SERVER_ADDRESS)
+#time.sleep(5) # Let the user actually see something!
+user_id = driver.find_element_by_id('id_login')
+user_pw = driver.find_element_by_id('id_password')
+user_id.send_keys(USER_ID)
+user_pw.send_keys(USER_PW)
+login_button = driver.find_element_by_css_selector('button[type="submit"]')
+login_button.click()
+create_new_button = driver.find_element_by_css_selector('.createNew')
+create_new_button.click()
+document_contents = driver.find_element_by_css_selector('.ProseMirror-content')
+time.sleep(3)
+document_contents.click()
+time.sleep(1)
+document_contents.click()
+for char in TEST_TEXT:
+    document_contents.send_keys(char)
+    time.sleep(randrange(1,20)/10.0)
+time.sleep(5) # Let the user actually see something!
+driver.quit()
+
+
+#driver.execute_script("window.open('https://www.google.com');")
